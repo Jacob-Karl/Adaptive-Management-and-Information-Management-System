7f310e8bfbea32f331d25ad1e59ecebb3445b05b
Jacob Karl <karljacobammon@gmail.com>
2021-11-24 10:39:45 -0800

    user functions 3

---
+++ user_functions/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from . import views
+
+
+app_name='user_functions'
+urlpatterns = [
+    url(r'^$', views.landing, name='landing'),
+    url(r'^login/$', views.login_view, name='login'),
+    url(r'^logout/$', views.logout_view, name='logout'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^hub/$', views.hub, name='hub'),
+    ]
