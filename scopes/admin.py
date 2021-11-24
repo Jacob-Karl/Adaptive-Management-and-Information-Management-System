f7d3c1e47101da50e845d94ff5794d35dabdd7e6
Jacob Karl <karljacobammon@gmail.com>
2021-11-24 10:32:48 -0800

    initial scopes upload

--- scopes/admin.py
+++ scopes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(SpeciesCommunity)
+admin.site.register(Location)
+admin.site.register(ConservationMeasure)
+admin.site.register(Goal)
