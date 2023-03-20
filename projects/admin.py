88a92400706e49fc7ff36887fa90b9d3fde01694
Jacob Karl <karljacobammon@gmail.com>
2023-03-20 09:33:02 -0700

    Search Update 2

--- projects/admin.py
+++ projects/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import *
 from reversion.admin import VersionAdmin
+from advanced_filters.admin import AdminAdvancedFiltersMixin
 
 #@admin.register(Project)
 class ClientModelAdmin(VersionAdmin):
@@ -8,19 +9,7 @@ class ClientModelAdmin(VersionAdmin):
 
 # Register your models here.
 @admin.register(Project)
-class ProjectAdmin(VersionAdmin): 
-    list_select_related = True    
-    
-    list_field = (
-        'WorktaskID',
-        'ProjectName',
-    )
-    
-    list_filter = (
-        'WorktaskID',
-        'ProjectName',
-    )
-    
+class ProjectAdmin(AdminAdvancedFiltersMixin, VersionAdmin): 
     search_fields = [
         'WorktaskID',
         'ProjectName',
@@ -51,18 +40,55 @@ class ProjectAdmin(VersionAdmin):
         'Objectives__ObjName',
         'RelatedProjects__WorktaskID',
         ]
+    
+    advanced_filter_fields = (
+        'WorktaskID',
+        'ProjectName',
+        'ProjectLead',
+        'ProjectStatus',
+        'ProjectType',
+        'ProjectStart',
+        'ProjectEnd',
+        'ProjectSummary',
+        'ProjectBackground',
+        'OtherConsMeas',
+        'OtherSpecies',
+        'Reference',
+        'ProjectContributors',
+        
+        'People__LastName',
+        'People__FirstName',
+        
+        'Goals__GoalName',
+        'SpeComs__Acronym',
+        'ConMeas__CMCode',
+        'Locations__LocationCode',
+        
+        'Triggers__TriggerName',
+        'Outputs__OutputTitle',
+        'Objectives__ObjCode',
+        
+        'Objectives__ObjName',
+        ('RelatedProjects__WorktaskID', 'Related Project ID'),            
+    )
 
 @admin.register(RelatedProject)
-class RelatedProjectAdmin(VersionAdmin):
+class RelatedProjectAdmin(AdminAdvancedFiltersMixin, VersionAdmin):
     search_fields = [
         'Project__WorktaskID',
         'Project__ProjectName',
         'WorktaskID',
         'RelationshipType',
     ]
+    advanced_filter_fields = (
+        'Project__WorktaskID',
+        'Project__ProjectName',
+        'WorktaskID',
+        'RelationshipType',
+        )
 
 @admin.register(Trigger)
-class TriggerAdmin(VersionAdmin):
+class TriggerAdmin(AdminAdvancedFiltersMixin, VersionAdmin):
     search_fields = [
         'ProjectID__WorktaskID',
         'ProjectID__ProjectName',        
@@ -73,9 +99,19 @@ class TriggerAdmin(VersionAdmin):
         'Reference',
         'TriggerStatus__StatusTrend',
     ]
+    advanced_filter_fields = (
+        'ProjectID__WorktaskID',
+        'ProjectID__ProjectName',        
+        'TriggerName',
+        'TriggerDescription',
+        'TriggerIndicators',
+        'ProposedResponse',
+        'Reference',
+        'TriggerStatus__StatusTrend',
+        )
 
 @admin.register(TriggerStatus)
-class TriggerStatusAdmin(VersionAdmin):
+class TriggerStatusAdmin(AdminAdvancedFiltersMixin, VersionAdmin):
     search_fields = [
         'TriggerID__TriggerName',
         'ReportingDate',
@@ -84,9 +120,17 @@ class TriggerStatusAdmin(VersionAdmin):
         'MgmtResponse',
         'Reference',
     ]
+    advanced_filter_fields = (
+        'TriggerID__TriggerName',
+        'ReportingDate',
+        'StatusTrend',
+        'MgmtInterp',
+        'MgmtResponse',
+        'Reference',
+        )
 
 @admin.register(Output)
-class OutputAdmin(VersionAdmin):
+class OutputAdmin(AdminAdvancedFiltersMixin, VersionAdmin):
     search_fields = [
         'ProjectID__WorktaskID',
         'ProjectID__ProjectName',
@@ -102,9 +146,24 @@ class OutputAdmin(VersionAdmin):
         'OutputConstraints',
         'Reference',
     ]
+    advanced_filter_fields = (
+        'ProjectID__WorktaskID',
+        'ProjectID__ProjectName',
+        'OutputType',
+        'OutputAuthors',
+        'OutputDate',
+        'OutputTitle',
+        'OutputVersion',
+        'OutputDescription',
+        'OutputDOI',
+        'OutputCitation',
+        'OutputURI',
+        'OutputConstraints',
+        'Reference',
+        )
 
 @admin.register(Objective)
-class ObjectiveAdmin(VersionAdmin):
+class ObjectiveAdmin(AdminAdvancedFiltersMixin, VersionAdmin):
     search_fields = [
         'ProjectID__WorktaskID',
         'ProjectID__ProjectName',
@@ -120,9 +179,24 @@ class ObjectiveAdmin(VersionAdmin):
         'Steps__StepName',
         'Steps__StepCode',
     ]
+    advanced_filter_fields = (
+        'ProjectID__WorktaskID',
+        'ProjectID__ProjectName',
+        'ObjCode',
+        'ObjName',
+        'ObjDescription',
+        'ObjStartDate',
+        'ObjEndDate',
+        'ObjFlowDiagram',
+        'Reference',
+        'Milestones__MilestoneID',
+        'Milestones__MilestoneName',
+        'Steps__StepName',
+        'Steps__StepCode',
+        )
 
 @admin.register(Milestone)
-class MilestoneAdmin(VersionAdmin):
+class MilestoneAdmin(AdminAdvancedFiltersMixin, VersionAdmin):
     search_fields = [
         'ObjectiveID__ObjCode',
         'ObjectiveID__ObjName',
@@ -133,9 +207,20 @@ class MilestoneAdmin(VersionAdmin):
         'MilestoneProgress__Status',
         'MilestoneProgress__Description',
     ]
+    advanced_filter_fields = (
+        'ObjectiveID__ObjCode',
+        'ObjectiveID__ObjName',
+        'MilestoneID',
+        'MilestoneName',
+        'Description',
+        'Reference',
+        'MilestoneProgress__Status',
+        'MilestoneProgress__Description',
+        )
+
 
 @admin.register(MilestoneProgress)
-class MilestoneProgressAdmin(VersionAdmin):
+class MilestoneProgressAdmin(AdminAdvancedFiltersMixin, VersionAdmin):
     search_fields = [
         'MilestoneID__MilestoneID',
         'MilestoneID__MilestoneName',
@@ -144,9 +229,17 @@ class MilestoneProgressAdmin(VersionAdmin):
         'Description',
         'Reference',
     ]
+    advanced_filter_fields = (
+        'MilestoneID__MilestoneID',
+        'MilestoneID__MilestoneName',
+        'ReportingDate',
+        'Status',
+        'Description',
+        'Reference',
+        )
 
 @admin.register(Step)
-class StepAdmin(VersionAdmin):
+class StepAdmin(AdminAdvancedFiltersMixin, VersionAdmin):
     search_fields = [
         'ObjectiveID__ObjCode',
         'ObjectiveID__ObjName',
@@ -161,9 +254,23 @@ class StepAdmin(VersionAdmin):
         'Methods__MethodTitle',
         'Methods__MethodCode', 
     ]
+    advanced_filter_fields = (
+        'ObjectiveID__ObjCode',
+        'ObjectiveID__ObjName',
+        'StepName',
+        'StepCode',
+        'StepType',
+        'StepSummary',
+        'StepStartDate',
+        'StepEndDate',
+        'StepDependencies',
+        'Reference',     
+        'Methods__MethodTitle',
+        'Methods__MethodCode', 
+        )
 
 @admin.register(Method)
-class MethodAdmin(VersionAdmin):
+class MethodAdmin(AdminAdvancedFiltersMixin, VersionAdmin):
     search_fields = [
         'StepID__StepName',
         'StepID__StepCode',
@@ -178,9 +285,23 @@ class MethodAdmin(VersionAdmin):
         'Protocols__ProtocolCode',
         'Protocols__ProtocolTitle',
     ]
+    advanced_filter_fields = (
+        'StepID__StepName',
+        'StepID__StepCode',
+        'MethodTitle',
+        'MethodCode',
+        'MethodType',
+        'MethodDate',
+        'MethodVersion',
+        'MethodDescription',
+        'MethodContact',
+        'Reference',
+        'Protocols__ProtocolCode',
+        'Protocols__ProtocolTitle',
+        )
 
 @admin.register(Protocol)
-class ProtocolAdmin(VersionAdmin):
+class ProtocolAdmin(AdminAdvancedFiltersMixin, VersionAdmin):
     search_fields = [
         'MethodID__MethodTitle',
         'MethodID__MethodCode',
@@ -193,3 +314,15 @@ class ProtocolAdmin(VersionAdmin):
         'ProtocolLink',
         'Reference',
     ]
+    advanced_filter_fields = (
+        'MethodID__MethodTitle',
+        'MethodID__MethodCode',
+        'ProtocolCode',
+        'ProtocolTitle',
+        'ProtocolVerision',
+        'ProtocolDate',
+        'ProtocolAuthor',
+        'ProtocolDescription',
+        'ProtocolLink',
+        'Reference',
+        )
